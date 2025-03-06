cf62dcfd3255cb0a9c46b437803739556259f116
BonelessImpl <bonelessimpl@gmail.com>
2025-03-06 13:00:26 +0400

    Appease clippy

--- tests/src/tests/utils.rs
+++ tests/src/tests/utils.rs
@@ -16,14 +16,14 @@ fn pips_borsch_serialization_back_and_forth() {
 
 #[rstest]
 #[trace]
-#[case(&[206, 137, 2, 0], 166350)]
-#[case(&[116, 38, 2, 0], 140916)]
-#[case(&[3, 186, 2, 0], 178691)]
-#[case(&[199, 66, 12, 0], 803527)]
-#[case(&[73, 131, 13, 0], 885577)]
-#[case(&[64, 66, 15, 0], 1000000)]
+#[case(&[206, 137, 2, 0], 166_350)]
+#[case(&[116, 38, 2, 0], 140_916)]
+#[case(&[3, 186, 2, 0], 178_691)]
+#[case(&[199, 66, 12, 0], 803_527)]
+#[case(&[73, 131, 13, 0], 885_577)]
+#[case(&[64, 66, 15, 0], 1_000_000)]
 #[case(&[0, 0, 0, 0], 0)]
 fn pip_borsch_deserialization_selected_values(#[case] serialized: &[u8], #[case] pips: u32) {
-    let deserialized: Pips = borsh::from_slice(&serialized).unwrap();
+    let deserialized: Pips = borsh::from_slice(serialized).unwrap();
     assert_eq!(deserialized, Pips::from_pips(pips).unwrap());
 }
