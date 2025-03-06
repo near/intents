94c6b248c10b213a446cdb1d407548bfcfcf9d75
BonelessImpl <bonelessimpl@gmail.com>
2025-03-06 13:24:25 +0400

    Add missing trace for rstest in pips tests

--- tests/src/tests/utils.rs
+++ tests/src/tests/utils.rs
@@ -17,11 +17,17 @@ fn pips_borsch_serialization_back_and_forth() {
 #[rstest]
 #[trace]
 #[case(&[206, 137, 2, 0], 166_350)]
+#[trace]
 #[case(&[116, 38, 2, 0], 140_916)]
+#[trace]
 #[case(&[3, 186, 2, 0], 178_691)]
+#[trace]
 #[case(&[199, 66, 12, 0], 803_527)]
+#[trace]
 #[case(&[73, 131, 13, 0], 885_577)]
+#[trace]
 #[case(&[64, 66, 15, 0], 1_000_000)]
+#[trace]
 #[case(&[0, 0, 0, 0], 0)]
 fn pip_borsch_deserialization_selected_values(#[case] serialized: &[u8], #[case] pips: u32) {
     let deserialized: Pips = borsh::from_slice(serialized).unwrap();
