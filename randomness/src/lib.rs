9519a61aa7648019e6f5767415825a421b5078e5
BonelessImpl <bonelessimpl@gmail.com>
2025-03-06 16:47:17 +0400

    Funnel randomness through a specific crate for good measure, and introduce deterministic randomness for tests

---
+++ randomness/src/lib.rs
@@ -0,0 +1,26 @@
+pub use rand::prelude::SliceRandom;
+pub use rand::{seq, CryptoRng, Rng, RngCore, SeedableRng};
+
+pub mod distributions {
+    pub use rand::distributions::{
+        Alphanumeric, DistString, Distribution, Standard, WeightedIndex,
+    };
+    pub mod uniform {
+        pub use rand::distributions::uniform::SampleRange;
+    }
+}
+
+pub mod rngs {
+    pub use rand::rngs::mock::StepRng;
+    pub use rand::rngs::OsRng;
+}
+
+#[must_use]
+pub fn make_true_rng() -> impl Rng + CryptoRng {
+    rand::rngs::StdRng::from_entropy()
+}
+
+#[must_use]
+pub fn make_pseudo_rng() -> impl Rng {
+    rand::rngs::ThreadRng::default()
+}
