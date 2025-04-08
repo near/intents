9b96252b27a537031c80381b28a840d50b23652d
BonelessImpl <bonelessimpl@gmail.com>
2025-04-08 13:15:10 +0400

    Implement ControllerUpgradable trait for PoA token

--- poa-token/src/contract.rs
+++ poa-token/src/contract.rs
@@ -1,8 +1,9 @@
 use crate::{CanWrapToken, PoaFungibleToken, UNWRAP_PREFIX, WITHDRAW_MEMO_PREFIX};
 use defuse_admin_utils::full_access_keys::FullAccessKeys;
+use defuse_controller::ControllerUpgradable;
 use defuse_near_utils::{
     CURRENT_ACCOUNT_ID, Lock, PREDECESSOR_ACCOUNT_ID, UnwrapOrPanic, UnwrapOrPanicCtx,
-    UnwrapOrPanicError,
+    UnwrapOrPanicError, method_name,
 };
 use near_contract_standards::{
     fungible_token::{
@@ -26,6 +27,9 @@ use near_sdk::{
     near, require, serde_json,
     store::Lazy,
 };
+
+const STATE_MIGRATE_FUNCTION: &str = method_name!(Contract::upgrade_to_versioned);
+const STATE_MIGRATE_DEFAULT_GAS: Gas = Gas::from_tgas(5);
 
 const FT_RESOLVE_UNWRAP_GAS: Gas = Gas::from_tgas(10);
 const DO_WRAP_TOKEN_GAS: Gas = Gas::from_tgas(10);
@@ -728,6 +732,30 @@ impl FullAccessKeys for Contract {
     fn delete_key(&mut self, public_key: PublicKey) -> Promise {
         Promise::new(CURRENT_ACCOUNT_ID.clone()).delete_key(public_key)
     }
+}
+
+#[near]
+impl ControllerUpgradable for Contract {
+    #[only(self, owner)]
+    #[payable]
+    fn upgrade(
+        &mut self,
+        #[serializer(borsh)] code: Vec<u8>,
+        #[serializer(borsh)] state_migration_gas: Option<Gas>,
+    ) -> Promise {
+        assert_one_yocto();
+        Promise::new(CURRENT_ACCOUNT_ID.clone())
+            .deploy_contract(code)
+            .function_call(
+                STATE_MIGRATE_FUNCTION.into(),
+                Vec::new(),
+                NearToken::from_yoctonear(0),
+                state_migration_gas.unwrap_or(STATE_MIGRATE_DEFAULT_GAS),
+            )
+    }
+
+    #[only(self, owner)]
+    fn state_migrate(&mut self) {}
 }
 
 #[derive(BorshSerialize, BorshStorageKey)]
