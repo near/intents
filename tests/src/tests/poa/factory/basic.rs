72d8a9c2c0c70e5d922a3b6638b5e22ce5529188
BonelessImpl <bonelessimpl@gmail.com>
2025-04-09 12:52:12 +0400

    Make some asserts more specific to errors

--- tests/src/tests/poa/factory/basic.rs
+++ tests/src/tests/poa/factory/basic.rs
@@ -29,20 +29,29 @@ async fn test_deploy_mint() {
 
     user.poa_factory_deploy_token(poa_factory.id(), "ft1", None)
         .await
-        .unwrap_err();
+        .unwrap_err()
+        .to_string();
 
-    root.poa_factory_deploy_token(poa_factory.id(), "ft1.abc", None)
-        .await
-        .unwrap_err();
+    assert!(
+        root.poa_factory_deploy_token(poa_factory.id(), "ft1.abc", None)
+            .await
+            .unwrap_err()
+            .to_string()
+            .contains("invalid token name")
+    );
 
     let ft1 = root
         .poa_factory_deploy_token(poa_factory.id(), "ft1", None)
         .await
         .unwrap();
 
-    root.poa_factory_deploy_token(poa_factory.id(), "ft1", None)
-        .await
-        .unwrap_err();
+    assert!(
+        root.poa_factory_deploy_token(poa_factory.id(), "ft1", None)
+            .await
+            .unwrap_err()
+            .to_string()
+            .contains("token exists")
+    );
 
     assert_eq!(
         sandbox.ft_token_balance_of(&ft1, user.id()).await.unwrap(),
@@ -54,9 +63,13 @@ async fn test_deploy_mint() {
         .await
         .unwrap();
 
-    user.poa_factory_ft_deposit(poa_factory.id(), "ft1", user.id(), 1000, None, None)
-        .await
-        .unwrap_err();
+    assert!(
+        user.poa_factory_ft_deposit(poa_factory.id(), "ft1", user.id(), 1000, None, None)
+            .await
+            .unwrap_err()
+            .to_string()
+            .contains("Requires one of these roles")
+    );
 
     root.poa_factory_ft_deposit(poa_factory.id(), "ft1", user.id(), 1000, None, None)
         .await
