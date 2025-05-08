3ccdd207285122b51330c9c6cd37afc55b5ec761
BonelessImpl <bonelessimpl@gmail.com>
2025-05-08 09:18:42 +0400

    Some minor refactoring for tests plus more nft transfers

--- tests/src/tests/defuse/tokens/nep171.rs
+++ tests/src/tests/defuse/tokens/nep171.rs
@@ -77,45 +77,94 @@ async fn transfer_nft_to_verifier(random_seed: Seed) {
     assert_eq!(nft2.owner_id, *env.user3.id());
 
     {
-        assert_eq!(nft1.owner_id, *env.user2.id());
-        assert!(
-            env.user2
-                .nft_transfer_call(
-                    nft_issuer_contract.id(),
-                    env.defuse.id(),
-                    nft1.token_id.clone(),
-                    None,
-                    env.user3.id().to_string(),
-                )
+        {
+            assert_eq!(nft1.owner_id, *env.user2.id());
+            assert!(
+                env.user2
+                    .nft_transfer_call(
+                        nft_issuer_contract.id(),
+                        env.defuse.id(),
+                        nft1.token_id.clone(),
+                        None,
+                        env.user3.id().to_string(),
+                    )
+                    .await
+                    .unwrap()
+            );
+
+            let nft1_data = env
+                .user2
+                .nft_token(nft_issuer_contract.id(), &nft1.token_id)
                 .await
                 .unwrap()
-        );
+                .unwrap();
+            assert_eq!(nft1_data.owner_id, *env.defuse.id());
+        }
 
-        let nft1_data = env
-            .user2
-            .nft_token(nft_issuer_contract.id(), &nft1.token_id)
-            .await
-            .unwrap()
-            .unwrap();
-        assert_eq!(nft1_data.owner_id, *env.defuse.id());
+        // Create the token id, expected inside the verifier contract
+        let nft1_mt_token_id =
+            MtTokenId::Nep171(nft_issuer_contract.id().to_owned(), nft1.token_id.clone());
+
+        // After transferring to defuse, the owner is user3, since it's specified in the message
+        assert_eq!(
+            env.defuse
+                .mt_balance_of(env.user2.id(), &nft1_mt_token_id.to_string())
+                .await
+                .unwrap(),
+            0
+        );
+        assert_eq!(
+            env.defuse
+                .mt_balance_of(env.user3.id(), &nft1_mt_token_id.to_string())
+                .await
+                .unwrap(),
+            1
+        );
     }
 
-    let nft1_mt_token_id =
-        MtTokenId::Nep171(nft_issuer_contract.id().to_owned(), nft1.token_id.clone());
+    {
+        {
+            assert_eq!(nft2.owner_id, *env.user3.id());
+            assert!(
+                env.user3
+                    .nft_transfer_call(
+                        nft_issuer_contract.id(),
+                        env.defuse.id(),
+                        nft2.token_id.clone(),
+                        None,
+                        env.user1.id().to_string(),
+                    )
+                    .await
+                    .unwrap()
+            );
 
-    // After transferring to defuse, the owner is user3, since it's specified in the message
-    assert_eq!(
-        env.defuse
-            .mt_balance_of(env.user2.id(), &nft1_mt_token_id.to_string())
-            .await
-            .unwrap(),
-        0
-    );
-    assert_eq!(
-        env.defuse
-            .mt_balance_of(env.user3.id(), &nft1_mt_token_id.to_string())
-            .await
-            .unwrap(),
-        1
-    );
+            let nft2_data = env
+                .user2
+                .nft_token(nft_issuer_contract.id(), &nft2.token_id)
+                .await
+                .unwrap()
+                .unwrap();
+            assert_eq!(nft2_data.owner_id, *env.defuse.id());
+        }
+
+        // Create the token id, expected inside the verifier contract
+        let nft2_mt_token_id =
+            MtTokenId::Nep171(nft_issuer_contract.id().to_owned(), nft2.token_id.clone());
+
+        // After transferring to defuse, the owner is user3, since it's specified in the message
+        assert_eq!(
+            env.defuse
+                .mt_balance_of(env.user3.id(), &nft2_mt_token_id.to_string())
+                .await
+                .unwrap(),
+            0
+        );
+        assert_eq!(
+            env.defuse
+                .mt_balance_of(env.user1.id(), &nft2_mt_token_id.to_string())
+                .await
+                .unwrap(),
+            1
+        );
+    }
 }
