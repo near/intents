12fe5e6b5b5520a1ff72826a80f6f254e6909c8d
De Fuse <fuzdefuz@gmail.com>
2025-05-23 04:53:58 +0400

    docs

--- core/src/payload/ton_connect.rs
+++ core/src/payload/ton_connect.rs
@@ -26,6 +26,10 @@ where
 
     fn extract_defuse_payload(self) -> Result<DefusePayload<T>, Self::Error> {
         if self.timestamp > defuse_near_utils::time::now() {
+            // TON Connect [specification](https://docs.tonconsole.com/academy/sign-data#in-a-smart-contract-on-chain)
+            // requires to check that "timestamp is recent". We don't have fixed TTL for
+            // off-chain signatures but rather check if `deadline` is not expired.
+            // So, it makes sense to at least check that the signature doesn't come from the future
             return Err(Error::custom("timestamp has not come yet"));
         }
         let TonConnectPayloadSchema::Text { text } = self.payload else {
