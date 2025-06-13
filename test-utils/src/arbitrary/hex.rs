f78f485220c8f1e27ad7f21139a6656307d94ecc
BonelessImpl <bonelessimpl@gmail.com>
2025-06-13 10:21:07 +0400

    Remove arbitrary array

--- test-utils/src/arbitrary/hex.rs
+++ test-utils/src/arbitrary/hex.rs
@@ -1,13 +1,10 @@
 use arbitrary::Arbitrary;
 use hex::ToHex;
 
-use crate::arbitrary::array::Array;
-
 /// N is the number of bytes, NOT hex characters
 pub fn arbitrary_hex<const N: usize>(
     u: &mut arbitrary::Unstructured<'_>,
 ) -> arbitrary::Result<String> {
-    let data = Array::<u8, N>::arbitrary(u)?;
-    let data: [u8; N] = data.into();
+    let data = <[u8; N]>::arbitrary(u)?;
     Ok(data.encode_hex())
 }
--- test-utils/src/arbitrary/mod.rs
+++ test-utils/src/arbitrary/mod.rs
@@ -1,3 +1,2 @@
 pub mod account_id;
-pub mod array;
 pub mod hex;
