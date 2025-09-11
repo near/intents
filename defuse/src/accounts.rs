92df8dc8491993d7482042cda60b72cd0fd1c9e0
hlgltvnnk <olalytvinenko@gmail.com>
2025-09-11 12:32:57 +0300

    changes according to the review #4: cleaning errors are ommited

--- defuse/src/accounts.rs
+++ defuse/src/accounts.rs
@@ -32,6 +32,8 @@ pub trait AccountManager {
     /// NOTE: MUST attach 1 yⓃ for security purposes.
     fn invalidate_nonces(&mut self, nonces: Vec<AsBase64<Nonce>>);
 
+    /// Clears all expired nonces for given accounts.
+    /// Omitting any errors, e.g. if account doesn't exist or nonces are not expired.
     /// NOTE: MUST attach 1 yⓃ for security purposes.
     fn clear_expired_nonces(&mut self, nonces: Vec<(AccountId, Vec<AsBase64<Nonce>>)>);
 
