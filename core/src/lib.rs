91cd08df0c36d67197c1c301a98cb33af423369f
pityjllk <pityjllk@proton.me>
2025-10-10 14:57:19 +0200

    move EventSink to dedicated file

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,13 +3,14 @@ pub mod amounts;
 mod deadline;
 pub mod engine;
 mod error;
+pub mod event_sink;
 pub mod events;
 pub mod fees;
 pub mod intents;
 mod nonce;
 pub mod payload;
 
-pub use self::{deadline::*, error::*, nonce::*};
+pub use self::{deadline::*, error::*, event_sink::*, nonce::*};
 
 pub use defuse_crypto as crypto;
 pub use defuse_erc191 as erc191;
@@ -18,40 +19,3 @@ pub use defuse_sep53 as sep53;
 pub use defuse_tip191 as tip191;
 pub use defuse_token_id as token_id;
 pub use defuse_ton_connect as ton_connect;
-
-pub enum EventMode {
-    Emit,
-    Record,
-}
-
-pub struct EventSink {
-    mode: EventMode,
-    events: Vec<events::DefuseEvent<'static>>,
-}
-
-impl Default for EventSink {
-    fn default() -> Self {
-        Self {
-            mode: EventMode::Emit,
-            events: Vec::new(),
-        }
-    }
-}
-
-impl EventSink {
-    pub fn consume_event(&mut self, event: events::DefuseEvent<'_>) {
-        match self.mode {
-            EventMode::Emit => event.emit(),
-            EventMode::Record => self.events.push(event.into_static()),
-        }
-    }
-
-    pub const fn record_only_mode(&mut self) {
-        self.mode = EventMode::Record;
-    }
-
-    #[allow(clippy::missing_const_for_fn)] // False positive: cannot be const due to Vec deref
-    pub fn recorded_events(&self) -> &[events::DefuseEvent<'static>] {
-        &self.events
-    }
-}
