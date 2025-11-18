f1e6094bfcc6819654fedd03bc125a96443325e7
pityjllk <pityjllk@proton.me>
2025-11-18 13:46:05 +0100

    improve readability

--- defuse/src/contract/tokens/nep141/deposit.rs
+++ defuse/src/contract/tokens/nep141/deposit.rs
@@ -5,7 +5,7 @@ use defuse_near_utils::{
 use defuse_nep245::receiver::ext_mt_receiver;
 use near_contract_standards::fungible_token::receiver::FungibleTokenReceiver;
 use near_plugins::{Pausable, pause};
-use near_sdk::{AccountId, PromiseOrValue, json_types::U128, near, require};
+use near_sdk::{AccountId, Promise, PromiseOrValue, json_types::U128, near, require};
 
 use crate::{
     contract::{Contract, ContractExt},
@@ -31,19 +31,14 @@ impl FungibleTokenReceiver for Contract {
 
         let token_id = CoreTokenId::Nep141(Nep141TokenId::new(PREDECESSOR_ACCOUNT_ID.clone()));
 
-        let DepositMessage {
-            receiver_id,
-            execute_intents,
-            refund_if_fails,
-            message,
-        } = if msg.is_empty() {
+        let deposit_message = if msg.is_empty() {
             DepositMessage::new(sender_id)
         } else {
             msg.parse().unwrap_or_panic_display()
         };
 
         self.deposit(
-            receiver_id.clone(),
+            deposit_message.receiver_id.clone(),
             [(
                 Nep141TokenId::new(PREDECESSOR_ACCOUNT_ID.clone()).into(),
                 amount_value,
@@ -52,32 +47,73 @@ impl FungibleTokenReceiver for Contract {
         )
         .unwrap_or_panic();
 
-        if message.as_ref().is_none_or(String::is_empty) {
-            return PromiseOrValue::Value(U128(0));
+        match &deposit_message {
+            DepositMessage {
+                execute_intents,
+                message: None,
+                ..
+            } if execute_intents.is_empty() => PromiseOrValue::Value(U128(0)),
+            DepositMessage {
+                refund_if_fails: true,
+                message: None,
+                ..
+            } => {
+                self.execute_intents(deposit_message.execute_intents);
+                PromiseOrValue::Value(U128(0))
+            }
+            DepositMessage {
+                refund_if_fails: false,
+                message: None,
+                ..
+            } => {
+                let _ = ext_intents::ext(CURRENT_ACCOUNT_ID.clone())
+                    .execute_intents(deposit_message.execute_intents);
+                PromiseOrValue::Value(U128(0))
+            }
+            DepositMessage {
+                message: Some(_), ..
+            } => self.handle_deposit_with_notification(deposit_message, token_id, amount_value),
         }
+    }
+}
 
-        let notification = ext_mt_receiver::ext(receiver_id.clone()).mt_on_transfer(
-            receiver_id.clone(),
-            vec![receiver_id.clone()],
-            vec![token_id.to_string()],
-            vec![U128(amount_value)],
-            message.unwrap(),
-        );
+impl Contract {
+    fn handle_deposit_with_notification(
+        &mut self,
+        deposit_message: DepositMessage,
+        token_id: CoreTokenId,
+        amount_value: u128,
+    ) -> PromiseOrValue<U128> {
+        let notification = ext_mt_receiver::ext(deposit_message.receiver_id.clone())
+            .mt_on_transfer(
+                deposit_message.receiver_id.clone(),
+                vec![deposit_message.receiver_id.clone()],
+                vec![token_id.to_string()],
+                vec![U128(amount_value)],
+                deposit_message.message.unwrap(),
+            );
 
         let resolution = Self::ext(CURRENT_ACCOUNT_ID.clone())
             .with_static_gas(Self::mt_resolve_deposit_gas(1))
             .with_unused_gas_weight(0)
-            .ft_resolve_deposit(&receiver_id, vec![token_id], vec![amount_value]);
+            .ft_resolve_deposit(
+                &deposit_message.receiver_id,
+                vec![token_id],
+                vec![amount_value],
+            );
 
-        if execute_intents.is_empty() {
+        if deposit_message.execute_intents.is_empty() {
             notification.then(resolution).into()
-        }else{
-            if refund_if_fails {
-                self.execute_intents(execute_intents);
+        } else {
+            if deposit_message.refund_if_fails {
+                self.execute_intents(deposit_message.execute_intents);
                 notification.then(resolution).into()
             } else {
-                ext_intents::ext(CURRENT_ACCOUNT_ID.clone()).execute_intents(execute_intents)
-                .then(notification).then(resolution).into()
+                ext_intents::ext(CURRENT_ACCOUNT_ID.clone())
+                    .execute_intents(deposit_message.execute_intents)
+                    .then(notification)
+                    .then(resolution)
+                    .into()
             }
         }
     }
