97375fe2d13b7170576a396485d7510845439e2f
pityjllk <pityjllk@proton.me>
2025-12-08 19:27:16 +0100

    compilesh

--- escrow-proxy/tests/transfer_auth.rs
+++ escrow-proxy/tests/transfer_auth.rs
@@ -2,10 +2,13 @@ mod env;
 
 use std::{collections::{HashMap, HashSet}, hash::Hash};
 
-use defuse_escrow_proxy::RolesConfig;
+use defuse_escrow_proxy::{ProxyConfig, RolesConfig};
 use defuse_sandbox::{Account, Sandbox};
 use defuse_transfer_auth::ext::{TransferAuthAccountExt, DefuseAccountExt};
 use env::AccountExt;
+use near_sdk::NearToken;
+
+const INIT_BALANCE: NearToken = NearToken::from_near(100);
 
 #[tokio::test]
 async fn test_deploy_transfer_auth_global_contract() {
@@ -13,33 +16,30 @@ async fn test_deploy_transfer_auth_global_contract() {
     let root = sandbox.root();
 
 
-    let (global_contract_id, wnear, defuse) = futures::join!(
+    let wnear = root.deploy_wnear("wnear").await;
+    let (transfer_auth_global,  defuse) = futures::join!(
         root.deploy_transfer_auth("global_transfer_auth"),
-        root.deploy_wnear("wnear"),
         root.deploy_verifier("defuse", wnear.id().clone()),
     );
 
+
+    let relay = root.create_subaccount("relay", INIT_BALANCE).await.unwrap();
 
     let roles = RolesConfig {
         super_admins: HashSet::from([root.id()]),
         admins: HashMap::new(),
         grantees: HashMap::new(),
     };
-    let proxy = sandbox.root().deploy_escrow_proxy("proxy").await;
 
-    // let config = ProxyConfig {
-    //     per_fill_global_contract_id: root.subaccount("per_fill_global_contract_id").id().clone(),
-    //     escrow_swap_global_contract_id: root
-    //         .subaccount("escrow_swap_global_contract_id")
-    //         .id()
-    //         .clone(),
-    //     auth_contract: root.subaccount("auth_contract").id().clone(),
-    //     auth_collee: root.subaccount("auth_collee").id().clone(),
-    // };
+    let config = ProxyConfig {
+        per_fill_global_contract_id: transfer_auth_global.clone(),
+        //NOTE: not really used in this test YET
+        escrow_swap_global_contract_id: transfer_auth_global.clone(),
+        auth_contract: defuse.id().clone(),
+        auth_collee: relay.id().clone(),
+    };
 
+    let proxy = sandbox.root().create_subaccount("proxy", INIT_BALANCE).await.unwrap();
+    let proxy = proxy.deploy_escrow_proxy(roles, config).await.unwrap();
 
-
-
-    let account = Account::new(global_contract_id, sandbox.root().network_config().clone());
-    assert!(account.exists().await, "global contract account should exist after deployment");
 }
