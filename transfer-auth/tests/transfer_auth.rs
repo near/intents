86c7dfc96e85bd5aea4e24defc2ab8edf8f4a0b7
pityjllk <pityjllk@proton.me>
2025-12-08 19:14:25 +0100

    env removed

--- transfer-auth/tests/transfer_auth.rs
+++ transfer-auth/tests/transfer_auth.rs
@@ -1,7 +1,7 @@
 use std::time::Duration;
 
-use defuse_sandbox::Sandbox;
-use defuse_transfer_auth::ext::{TransferAuthAccountExt, DefuseAccountExt};
+use defuse_sandbox::{Account, Sandbox};
+use defuse_transfer_auth::ext::TransferAuthAccountExt;
 use defuse_transfer_auth::storage::{ContractStorage, State};
 use near_sdk::{
     Gas, NearToken,
@@ -86,248 +86,244 @@ async fn transfer_auth_global_deployment() {
     assert_eq!(cotnract_instance1_state.state_init.solver_id, solver1.id());
 }
 
-// #[tokio::test]
-// async fn on_auth_call() {
-//     let env = BaseEnv::new().await.unwrap();
-//     let root = env.root();
-//
-//     let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
-//         root.create_subaccount("solver", INIT_BALANCE),
-//         root.create_subaccount("escrow", INIT_BALANCE),
-//         root.create_subaccount("auth-contract", INIT_BALANCE),
-//         root.create_subaccount("auth-callee", INIT_BALANCE),
-//         root.create_subaccount("proxy", INIT_BALANCE),
-//     )
-//     .unwrap();
-//
-//     let state = State {
-//         solver_id: solver.id(),
-//         escrow_contract_id: escrow.id(),
-//         auth_contract: auth_contract.id(),
-//         auth_callee: relay.id(),
-//         querier: proxy.id(),
-//         msg_hash: [0; 32],
-//     };
-//
-//     let transfer_auth_instance = root
-//         .deploy_transfer_auth_instance(env.transfer_auth_global.clone(), state)
-//         .await;
-//     let instance_state = proxy
-//         .tx(transfer_auth_instance.clone())
-//         .function_call_json::<ContractStorage>(
-//             "state",
-//             "{}",
-//             Gas::from_tgas(300),
-//             NearToken::from_near(0),
-//         )
-//         .await
-//         .unwrap();
-//
-//     // unauthorized contract (relay vs auth_contract)
-//     relay
-//         .tx(transfer_auth_instance.clone())
-//         .function_call_json::<()>(
-//             "on_auth",
-//             json!({ "signer_id": relay.id(), "msg": "" }),
-//             Gas::from_tgas(300),
-//             NearToken::from_near(0),
-//         )
-//         .await
-//         .unwrap_err();
-//
-//     // unauthorized callee (auth_contract vs relay)
-//     auth_contract
-//         .tx(transfer_auth_instance.clone())
-//         .function_call_json::<()>(
-//             "on_auth",
-//             json!({ "signer_id": auth_contract.id(), "msg": "" }),
-//             Gas::from_tgas(300),
-//             NearToken::from_near(0),
-//         )
-//         .await
-//         .unwrap_err();
-//
-//     auth_contract
-//         .tx(transfer_auth_instance.clone())
-//         .function_call_json::<()>(
-//             "on_auth",
-//             json!({ "signer_id": relay.id(), "msg": "" }),
-//             Gas::from_tgas(300),
-//             NearToken::from_near(0),
-//         )
-//         .await
-//         .unwrap();
-// }
-//
-// #[tokio::test]
-// async fn transfer_auth_early_authorization() {
-//     let env = BaseEnv::new().await.unwrap();
-//     let root = env.root();
-//
-//     let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
-//         root.create_subaccount("solver", INIT_BALANCE),
-//         root.create_subaccount("escrow", INIT_BALANCE),
-//         root.create_subaccount("auth-contract", INIT_BALANCE),
-//         root.create_subaccount("auth-callee", INIT_BALANCE),
-//         root.create_subaccount("proxy", INIT_BALANCE),
-//     )
-//     .unwrap();
-//
-//     let state = State {
-//         solver_id: solver.id(),
-//         escrow_contract_id: escrow.id(),
-//         auth_contract: auth_contract.id(),
-//         auth_callee: relay.id(),
-//         querier: proxy.id(),
-//         msg_hash: [0; 32],
-//     };
-//
-//     let transfer_auth_instance = root
-//         .deploy_transfer_auth_instance(env.transfer_auth_global.clone(), state)
-//         .await;
-//
-//
-//     assert!(env.account_exists(transfer_auth_instance.clone()).await);
-//
-//     auth_contract
-//         .tx(transfer_auth_instance.clone())
-//         .function_call_json::<()>(
-//             "on_auth",
-//             json!({ "signer_id": relay.id(), "msg": "" }),
-//             Gas::from_tgas(300),
-//             NearToken::from_near(0),
-//         )
-//         .await
-//         .unwrap();
-//
-//     assert_eq!(
-//         proxy
-//             .tx(transfer_auth_instance.clone())
-//             .function_call_json::<bool>(
-//                 "wait_for_authorization",
-//                 json!({}),
-//                 Gas::from_tgas(300),
-//                 NearToken::from_near(0),
-//             )
-//             .await
-//             .unwrap(),
-//         true
-//     );
-//
-//
-//     assert!(!env.account_exists(transfer_auth_instance.clone()).await);
-// }
-//
-// #[tokio::test]
-// async fn transfer_auth_async_authorization() {
-//     let env = BaseEnv::new().await.unwrap();
-//     let root = env.root();
-//
-//     let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
-//         root.create_subaccount("solver", INIT_BALANCE),
-//         root.create_subaccount("escrow", INIT_BALANCE),
-//         root.create_subaccount("auth-contract", INIT_BALANCE),
-//         root.create_subaccount("auth-callee", INIT_BALANCE),
-//         root.create_subaccount("proxy", INIT_BALANCE),
-//     )
-//     .unwrap();
-//
-//     let state = State {
-//         solver_id: solver.id(),
-//         escrow_contract_id: escrow.id(),
-//         auth_contract: auth_contract.id(),
-//         auth_callee: relay.id(),
-//         querier: proxy.id(),
-//         msg_hash: [0; 32],
-//     };
-//
-//     let transfer_auth_instance = root
-//         .deploy_transfer_auth_instance(env.transfer_auth_global.clone(), state)
-//         .await;
-//
-//     let authorized = tokio::spawn({
-//         let transfer_auth_instance = transfer_auth_instance.clone();
-//         async move {
-//             proxy
-//                 .tx(transfer_auth_instance.clone())
-//                 .function_call_json::<bool>(
-//                     "wait_for_authorization",
-//                     json!({}),
-//                     Gas::from_tgas(300),
-//                     NearToken::from_near(0),
-//                 )
-//                 .await
-//                 .unwrap()
-//         }
-//     });
-//
-//     // replace with waiting for couple blocks
-//     tokio::time::sleep(Duration::from_secs(3)).await;
-//
-//     assert!(env.account_exists(transfer_auth_instance.clone()).await);
-//     auth_contract
-//         .tx(transfer_auth_instance.clone())
-//         .function_call_json::<()>(
-//             "on_auth",
-//             json!({ "signer_id": relay.id(), "msg": "" }),
-//             Gas::from_tgas(300),
-//             NearToken::from_near(0),
-//         )
-//         .await
-//         .unwrap();
-//
-//     assert!(authorized.await.unwrap());
-//
-//     assert!(!env.account_exists(transfer_auth_instance.clone()).await);
-// }
-//
-// #[tokio::test]
-// async fn transfer_auth_async_authorization_timeout() {
-//     let env = BaseEnv::new().await.unwrap();
-//     let root = env.root();
-//
-//     let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
-//         root.create_subaccount("solver", INIT_BALANCE),
-//         root.create_subaccount("escrow", INIT_BALANCE),
-//         root.create_subaccount("auth-contract", INIT_BALANCE),
-//         root.create_subaccount("auth-callee", INIT_BALANCE),
-//         root.create_subaccount("proxy", INIT_BALANCE),
-//     )
-//     .unwrap();
-//
-//     let state = State {
-//         solver_id: solver.id(),
-//         escrow_contract_id: escrow.id(),
-//         auth_contract: auth_contract.id(),
-//         auth_callee: relay.id(),
-//         querier: proxy.id(),
-//         msg_hash: [0; 32],
-//     };
-//
-//     let transfer_auth_instance = root
-//         .deploy_transfer_auth_instance(env.transfer_auth_global.clone(), state)
-//         .await;
-//
-//     let wait_for_authorization = proxy
-//         .tx(transfer_auth_instance.clone())
-//         .function_call_json::<bool>(
-//             "wait_for_authorization",
-//             json!({}),
-//             Gas::from_tgas(300),
-//             NearToken::from_near(0),
-//         );
-//
-//     let forward_time = env.fast_forward(200);
-//
-//
-//
-//     assert!(env.account_exists(transfer_auth_instance.clone()).await);
-//     let (authorized, _) = futures::join!(
-//         async { wait_for_authorization.await },
-//         forward_time
-//     );
-//
-//     assert!(!authorized.unwrap());
-//
-//     assert!(!env.account_exists(transfer_auth_instance.clone()).await);
-// }
+#[tokio::test]
+async fn on_auth_call() {
+    let sandbox = Sandbox::new().await;
+    let root = sandbox.root();
+
+    let transfer_auth_global = root.deploy_transfer_auth("auth").await;
+
+    let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
+        root.create_subaccount("solver", INIT_BALANCE),
+        root.create_subaccount("escrow", INIT_BALANCE),
+        root.create_subaccount("auth-contract", INIT_BALANCE),
+        root.create_subaccount("auth-callee", INIT_BALANCE),
+        root.create_subaccount("proxy", INIT_BALANCE),
+    )
+    .unwrap();
+
+    let state = State {
+        solver_id: solver.id(),
+        escrow_contract_id: escrow.id(),
+        auth_contract: auth_contract.id(),
+        auth_callee: relay.id(),
+        querier: proxy.id(),
+        msg_hash: [0; 32],
+    };
+
+    let transfer_auth_instance = root
+        .deploy_transfer_auth_instance(transfer_auth_global.clone(), state)
+        .await;
+
+    // unauthorized contract (relay vs auth_contract)
+    relay
+        .tx(transfer_auth_instance.clone())
+        .function_call_json::<()>(
+            "on_auth",
+            json!({ "signer_id": relay.id(), "msg": "" }),
+            Gas::from_tgas(300),
+            NearToken::from_near(0),
+        )
+        .await
+        .unwrap_err();
+
+    // unauthorized callee (auth_contract vs relay)
+    auth_contract
+        .tx(transfer_auth_instance.clone())
+        .function_call_json::<()>(
+            "on_auth",
+            json!({ "signer_id": auth_contract.id(), "msg": "" }),
+            Gas::from_tgas(300),
+            NearToken::from_near(0),
+        )
+        .await
+        .unwrap_err();
+
+    auth_contract
+        .tx(transfer_auth_instance.clone())
+        .function_call_json::<()>(
+            "on_auth",
+            json!({ "signer_id": relay.id(), "msg": "" }),
+            Gas::from_tgas(300),
+            NearToken::from_near(0),
+        )
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn transfer_auth_early_authorization() {
+    let sandbox = Sandbox::new().await;
+    let root = sandbox.root();
+
+    let transfer_auth_global = root.deploy_transfer_auth("auth").await;
+
+    let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
+        root.create_subaccount("solver", INIT_BALANCE),
+        root.create_subaccount("escrow", INIT_BALANCE),
+        root.create_subaccount("auth-contract", INIT_BALANCE),
+        root.create_subaccount("auth-callee", INIT_BALANCE),
+        root.create_subaccount("proxy", INIT_BALANCE),
+    )
+    .unwrap();
+
+    let state = State {
+        solver_id: solver.id(),
+        escrow_contract_id: escrow.id(),
+        auth_contract: auth_contract.id(),
+        auth_callee: relay.id(),
+        querier: proxy.id(),
+        msg_hash: [0; 32],
+    };
+
+    let transfer_auth_instance = root
+        .deploy_transfer_auth_instance(transfer_auth_global.clone(), state)
+        .await;
+
+    assert!(Account::new(transfer_auth_instance.clone(), root.network_config().clone()).exists().await);
+
+    auth_contract
+        .tx(transfer_auth_instance.clone())
+        .function_call_json::<()>(
+            "on_auth",
+            json!({ "signer_id": relay.id(), "msg": "" }),
+            Gas::from_tgas(300),
+            NearToken::from_near(0),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(
+        proxy
+            .tx(transfer_auth_instance.clone())
+            .function_call_json::<bool>(
+                "wait_for_authorization",
+                json!({}),
+                Gas::from_tgas(300),
+                NearToken::from_near(0),
+            )
+            .await
+            .unwrap(),
+        true
+    );
+
+    assert!(!Account::new(transfer_auth_instance.clone(), root.network_config().clone()).exists().await);
+}
+
+#[tokio::test]
+async fn transfer_auth_async_authorization() {
+    let sandbox = Sandbox::new().await;
+    let root = sandbox.root();
+
+    let transfer_auth_global = root.deploy_transfer_auth("auth").await;
+
+    let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
+        root.create_subaccount("solver", INIT_BALANCE),
+        root.create_subaccount("escrow", INIT_BALANCE),
+        root.create_subaccount("auth-contract", INIT_BALANCE),
+        root.create_subaccount("auth-callee", INIT_BALANCE),
+        root.create_subaccount("proxy", INIT_BALANCE),
+    )
+    .unwrap();
+
+    let state = State {
+        solver_id: solver.id(),
+        escrow_contract_id: escrow.id(),
+        auth_contract: auth_contract.id(),
+        auth_callee: relay.id(),
+        querier: proxy.id(),
+        msg_hash: [0; 32],
+    };
+
+    let transfer_auth_instance = root
+        .deploy_transfer_auth_instance(transfer_auth_global.clone(), state)
+        .await;
+
+    let network_config = root.network_config().clone();
+    let authorized = tokio::spawn({
+        let transfer_auth_instance = transfer_auth_instance.clone();
+        async move {
+            proxy
+                .tx(transfer_auth_instance.clone())
+                .function_call_json::<bool>(
+                    "wait_for_authorization",
+                    json!({}),
+                    Gas::from_tgas(300),
+                    NearToken::from_near(0),
+                )
+                .await
+                .unwrap()
+        }
+    });
+
+    // replace with waiting for couple blocks
+    tokio::time::sleep(Duration::from_secs(3)).await;
+
+    assert!(Account::new(transfer_auth_instance.clone(), network_config.clone()).exists().await);
+    auth_contract
+        .tx(transfer_auth_instance.clone())
+        .function_call_json::<()>(
+            "on_auth",
+            json!({ "signer_id": relay.id(), "msg": "" }),
+            Gas::from_tgas(300),
+            NearToken::from_near(0),
+        )
+        .await
+        .unwrap();
+
+    assert!(authorized.await.unwrap());
+
+    assert!(!Account::new(transfer_auth_instance.clone(), network_config.clone()).exists().await);
+}
+
+#[tokio::test]
+async fn transfer_auth_async_authorization_timeout() {
+    let sandbox = Sandbox::new().await;
+    let root = sandbox.root();
+
+    let transfer_auth_global = root.deploy_transfer_auth("auth").await;
+
+    let (solver, escrow, auth_contract, relay, proxy) = futures::try_join!(
+        root.create_subaccount("solver", INIT_BALANCE),
+        root.create_subaccount("escrow", INIT_BALANCE),
+        root.create_subaccount("auth-contract", INIT_BALANCE),
+        root.create_subaccount("auth-callee", INIT_BALANCE),
+        root.create_subaccount("proxy", INIT_BALANCE),
+    )
+    .unwrap();
+
+    let state = State {
+        solver_id: solver.id(),
+        escrow_contract_id: escrow.id(),
+        auth_contract: auth_contract.id(),
+        auth_callee: relay.id(),
+        querier: proxy.id(),
+        msg_hash: [0; 32],
+    };
+
+    let transfer_auth_instance = root
+        .deploy_transfer_auth_instance(transfer_auth_global.clone(), state)
+        .await;
+
+    let network_config = root.network_config().clone();
+    let wait_for_authorization = proxy
+        .tx(transfer_auth_instance.clone())
+        .function_call_json::<bool>(
+            "wait_for_authorization",
+            json!({}),
+            Gas::from_tgas(300),
+            NearToken::from_near(0),
+        );
+
+    let forward_time = sandbox.fast_forward(200);
+
+    assert!(Account::new(transfer_auth_instance.clone(), network_config.clone()).exists().await);
+    let (authorized, _) = futures::join!(
+        async { wait_for_authorization.await },
+        forward_time
+    );
+
+    assert!(!authorized.unwrap());
+
+    assert!(!Account::new(transfer_auth_instance.clone(), network_config.clone()).exists().await);
+}
