033dedac79b4b43349062842d9373bcd93e2d983
hlgltvnnk <olalytvinenko@gmail.com>
2025-12-09 15:30:20 +0200

    fix

--- sandbox/src/helpers.rs
+++ sandbox/src/helpers.rs
@@ -7,9 +7,11 @@ use std::{fs, path::Path};
 
 pub fn read_wasm(name: impl AsRef<Path>) -> Vec<u8> {
     let filename = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("../res/")
+        .join("../")
         .join(name)
         .with_extension("wasm");
+
+    println!("FILEPATH: {filename:?}",);
 
     fs::read(filename).unwrap()
 }
