fbf29053aeb918ba0fdb63d4d754eb0f99154d82
pityjllk <pityjllk@proton.me>
2026-01-07 16:18:12 +0100

    organize code

--- escrow-proxy/src/contract/mod.rs
+++ escrow-proxy/src/contract/mod.rs
@@ -1,25 +1,19 @@
 mod admin;
 mod tokens;
+mod utils;
 mod upgrade;
 
 use core::iter;
-use std::borrow::Cow;
 
 use defuse_escrow_swap::ext_escrow;
-use defuse_near_utils::UnwrapOrPanicError;
-use defuse_oneshot_condvar::{
-    CondVarContext, ext_oneshot_condvar,
-    storage::{ContractStorage, StateInit as CondVarStateInit},
-};
+use defuse_oneshot_condvar::{CondVarContext, ext_oneshot_condvar};
 use near_plugins::{AccessControlRole, AccessControllable, access_control, access_control_any};
 use near_sdk::{
-    AccountId, CryptoHash, Gas, NearToken, PanicOnDefault, Promise, PromiseResult, env,
-    json_types::U128,
-    near, require, serde_json,
+    AccountId, CryptoHash, Gas, NearToken, PanicOnDefault, Promise, env, near, require,
     state_init::{StateInit, StateInitV1},
 };
 
-use crate::message::{EscrowParams, TransferMessage};
+use crate::message::EscrowParams;
 use crate::state::{ProxyConfig, RolesConfig};
 use crate::{EscrowProxy, Role, RoleFlags};
 
@@ -28,62 +22,6 @@ use crate::{EscrowProxy, Role, RoleFlags};
 #[derive(PanicOnDefault)]
 pub struct Contract {
     config: ProxyConfig,
-}
-
-impl Contract {
-    fn get_deterministic_transfer_auth_state_init(&self, msg_hash: [u8; 32]) -> StateInit {
-        let state = CondVarStateInit {
-            escrow_contract_id: self.config.escrow_swap_contract_id.clone(),
-            auth_contract: self.config.auth_contract.clone(),
-            on_auth_signer: self.config.auth_collee.clone(),
-            authorizee: env::current_account_id(),
-            msg_hash,
-        };
-
-        StateInit::V1(StateInitV1 {
-            code: self.config.per_fill_contract_id.clone(),
-            //TODO: get rid of unwrap
-            data: ContractStorage::init_state(state).unwrap(),
-        })
-    }
-
-    /// Creates the authorization promise for token transfers.
-    /// Returns the parsed TransferMessage and the auth call Promise.
-    fn create_auth_call(
-        &self,
-        sender_id: &AccountId,
-        token_ids: &[defuse_nep245::TokenId],
-        amounts: &[U128],
-        msg: &str,
-    ) -> (TransferMessage, Promise) {
-        let transfer_message: TransferMessage = msg.parse().unwrap_or_panic_display();
-
-        let context_hash = CondVarContext {
-            sender_id: Cow::Borrowed(sender_id),
-            token_ids: Cow::Borrowed(token_ids),
-            amounts: Cow::Borrowed(amounts),
-            salt: transfer_message.salt,
-            msg: Cow::Borrowed(msg),
-        }
-        .hash();
-
-        let auth_contract_state_init =
-            self.get_deterministic_transfer_auth_state_init(context_hash);
-        let auth_contract_id = auth_contract_state_init.derive_account_id();
-        let auth_call = Promise::new(auth_contract_id)
-            .state_init(auth_contract_state_init, NearToken::from_near(0));
-
-        (transfer_message, auth_call)
-    }
-
-    fn parse_authorization_result() -> bool {
-        match env::promise_result(0) {
-            PromiseResult::Successful(value) => {
-                serde_json::from_slice::<bool>(&value).unwrap_or(false)
-            }
-            PromiseResult::Failed => false,
-        }
-    }
 }
 
 #[near]
