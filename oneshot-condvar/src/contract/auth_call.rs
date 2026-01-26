fb3d41b927a2cc38828658502c991049595092b3
pityjllk <pityjllk@proton.me>
2026-01-26 11:12:55 +0100

    const messages

--- oneshot-condvar/src/contract/auth_call.rs
+++ oneshot-condvar/src/contract/auth_call.rs
@@ -3,20 +3,24 @@ use near_sdk::{AccountId, PromiseOrValue, env, near, require};
 
 use super::{Contract, ContractExt};
 
+const ERR_MSG_NOT_EMPTY: &str = "message must be empty";
+const ERR_WRONG_AUTH_CALLER: &str = "Unauthorized auth contract";
+const ERR_WRONG_SIGNER: &str = "Unauthorized on_auth signer";
+
 #[near]
 impl AuthCallee for Contract {
     #[payable]
     fn on_auth(&mut self, signer_id: AccountId, msg: String) -> PromiseOrValue<()> {
-        require!(msg.is_empty(), "message must be empty");
+        require!(msg.is_empty(), ERR_MSG_NOT_EMPTY);
 
         let state = self.0.try_as_alive();
         require!(
             env::predecessor_account_id() == state.state_init.auth_contract,
-            "Unauthorized auth contract"
+            ERR_WRONG_AUTH_CALLER
         );
         require!(
             signer_id == state.state_init.on_auth_signer,
-            "Unauthorized on_auth signer"
+            ERR_WRONG_SIGNER
         );
 
         self.do_notify();
