b663e2c4de0c1d783c8fd63f91dd1f13e3dd1060
pityjllk <pityjllk@proton.me>
2026-01-26 11:43:20 +0100

    remove init attribute

--- oneshot-condvar/src/contract/mod.rs
+++ oneshot-condvar/src/contract/mod.rs
@@ -30,7 +30,6 @@ impl ContractStorage {
 
 #[near]
 impl Contract {
-    #[init]
     #[allow(clippy::missing_const_for_fn, clippy::use_self)]
     pub fn new(state_init: StateInit) -> Self {
         Self(ContractStorage::init(state_init))
