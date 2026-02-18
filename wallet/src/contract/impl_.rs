f7b0ef8ea3c64f20dda91836ff697687f2b8083d
Arseny Mitin <mitinarseny@gmail.com>
2026-02-18 19:06:40 +0400

    fix: renaming

--- wallet/src/contract/impl_.rs
+++ wallet/src/contract/impl_.rs
@@ -118,7 +118,7 @@ contract_impl! {
             /// 1. Authenticators are general-purpose signers and they usually implement
             ///   blind singing.
             /// 2. This reduces length of the `proof` submitted on-chain.
-            type SigningStandard = Borsh<WalletDomain<Sha256<Webauthn<Ed25519>>>>;
+            type SigningStandard = Borsh<DomainPrefix<Sha256<Webauthn<Ed25519>>>>;
         }
     }
 
@@ -141,7 +141,7 @@ contract_impl! {
             /// 1. Authenticators are general-purpose signers and they usually implement
             ///   blind singing.
             /// 2. This reduces length of the `proof` submitted on-chain.
-            type SigningStandard = Borsh<WalletDomain<Sha256<Webauthn<P256>>>>;
+            type SigningStandard = Borsh<DomainPrefix<Sha256<Webauthn<P256>>>>;
         }
 
     }
