ae627b2d6456862a3e54bb5a21dc521234152006
pityjllk <pityjllk@proton.me>
2026-03-04 10:20:20 +0100

    chore: cargo-near upgrade - use prebuilt binary (#217)
    * use prebuilt binary
    
    * bump rust image
    
    * fix formatting
    
    * fix escrow swap
    
    * add near-sdk/__abi-generate
    
    * near-sdk/__abi-generate => near-sdk/abi

--- token-id/src/lib.rs
+++ token-id/src/lib.rs
@@ -58,7 +58,7 @@ pub use self::error::TokenIdError;
     ),
     strum(serialize_all = "snake_case"),
     cfg_attr(
-        all(feature = "abi", not(target_arch = "wasm32")),
+        feature = "abi",
         derive(::near_sdk::NearSchema),
         schemars(with = "String"),
     ),
@@ -130,7 +130,7 @@ impl FromStr for TokenId {
     }
 }
 
-#[cfg(all(feature = "abi", not(target_arch = "wasm32")))]
+#[cfg(feature = "abi")]
 const _: () = {
     use near_sdk::schemars::{
         JsonSchema,
