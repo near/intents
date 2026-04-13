6a646ef1d07993235645aa61cbb61dea736a772a
hlgltvnnk <olalytvinenko@gmail.com>
2026-04-13 12:36:40 +0300

    wit updated

--- crates/outlayer/sdk/src/ed25519.rs
+++ crates/outlayer/sdk/src/ed25519.rs
@@ -12,24 +12,18 @@ wit_bindgen::generate!({
 impl Ed25519Host for SysHost {
     fn ed25519_get_project_public_key() -> Ed25519PublicKey {
         ed25519::get_project_public_key()
-            .expect("failed to get project public key")
-            .bytes
             .try_into()
             .expect("public key must be 32 bytes")
     }
 
     fn ed25519_derive_public_key(path: impl AsRef<str>) -> Ed25519PublicKey {
         ed25519::derive_public_key(path.as_ref())
-            .expect("failed to derive public key")
-            .bytes
             .try_into()
             .expect("public key must be 32 bytes")
     }
 
     fn ed25519_sign(path: impl AsRef<str>, msg: impl AsRef<[u8]>) -> Ed25519Signature {
         ed25519::sign(path.as_ref(), msg.as_ref())
-            .expect("failed to sign message")
-            .bytes
             .try_into()
             .expect("signature must be 64 bytes")
     }
--- crates/outlayer/sdk/src/secp256k1.rs
+++ crates/outlayer/sdk/src/secp256k1.rs
@@ -12,24 +12,18 @@ wit_bindgen::generate!({
 impl Secp256k1Host for SysHost {
     fn secp256k1_get_project_public_key() -> Secp256k1PublicKey {
         secp256k1::get_project_public_key()
-            .expect("failed to get project public key")
-            .bytes
             .try_into()
             .expect("public key must be 64 bytes")
     }
 
     fn secp256k1_derive_public_key(path: impl AsRef<str>) -> Secp256k1PublicKey {
         secp256k1::derive_public_key(path.as_ref())
-            .expect("failed to derive public key")
-            .bytes
             .try_into()
             .expect("public key must be 64 bytes")
     }
 
     fn secp256k1_sign(path: impl AsRef<str>, msg: impl AsRef<[u8]>) -> Secp256k1Signature {
         secp256k1::sign(path.as_ref(), msg.as_ref())
-            .expect("failed to sign message")
-            .bytes
             .try_into()
             .expect("signature must be 65 bytes")
     }
