90811a3d3b2dd99f7f5ec0637604882d7fc1ed6a
hlgltvnnk <olalytvinenko@gmail.com>
2026-04-14 16:23:25 +0300

    chores

--- crates/outlayer/sys/src/crypto/mod.rs
+++ crates/outlayer/sys/src/crypto/mod.rs
@@ -1,11 +1,9 @@
 #[cfg(feature = "ed25519")]
-mod ed25519;
+mod ed25519_host;
 #[cfg(feature = "secp256k1")]
-mod secp256k1;
+mod secp256k1_host;
 
-pub mod crypto {
-    #[cfg(feature = "ed25519")]
-    pub use super::ed25519::ed25519;
-    #[cfg(feature = "secp256k1")]
-    pub use super::secp256k1::secp256k1;
-}
+#[cfg(feature = "ed25519")]
+pub use ed25519_host::ed25519;
+#[cfg(feature = "secp256k1")]
+pub use secp256k1_host::secp256k1;
