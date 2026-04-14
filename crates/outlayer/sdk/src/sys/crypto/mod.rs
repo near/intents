b573e20a89e3e55d10310305c4c60f101925b63e
hlgltvnnk <olalytvinenko@gmail.com>
2026-04-14 16:27:53 +0300

    chores

---
+++ crates/outlayer/sdk/src/sys/crypto/mod.rs
@@ -0,0 +1,4 @@
+#[cfg(feature = "ed25519")]
+mod ed25519;
+#[cfg(feature = "secp256k1")]
+mod secp256k1;
--- crates/outlayer/sdk/src/sys/mod.rs
+++ crates/outlayer/sdk/src/sys/mod.rs
@@ -1,6 +1,3 @@
-pub struct SysHost;
+pub mod crypto;
 
-#[cfg(feature = "ed25519")]
-mod ed25519;
-#[cfg(feature = "secp256k1")]
-mod secp256k1;
+pub struct SysHost;
