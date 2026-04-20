44840f0d7ec9cea8f9299101cd537638151b175e
Arseny Mitin <mitinarseny@gmail.com>
2026-04-20 16:48:19 +0400

    fix

--- crates/outlayer/sdk/src/host/crypto/secp256k1.rs
+++ crates/outlayer/sdk/src/host/crypto/secp256k1.rs
@@ -12,7 +12,7 @@ pub fn derive_public_key(path: impl AsRef<str>) -> Secp256k1PublicKey {
 
 pub fn sign(path: impl AsRef<str>, msg: impl AsRef<[u8]>) -> Secp256k1Signature {
     #[cfg(target_family = "wasm")]
-    return ::defuse_outlayer_sys::crypto::ed25519::sign(path.as_ref(), msg.as_ref())
+    return ::defuse_outlayer_sys::crypto::secp256k1::sign(path.as_ref(), msg.as_ref())
         .try_into()
         .expect("secp256k1 signature must be 65 bytes");
 
