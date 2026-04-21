b5e25dc7c6424dd263bb502958b2b5adf40e7294
Arseny Mitin <mitinarseny@gmail.com>
2026-04-22 03:56:05 +0400

    wip

--- crates/outlayer/worker-host/src/crypto/secp256k1.rs
+++ crates/outlayer/worker-host/src/crypto/secp256k1.rs
@@ -1,12 +1,28 @@
 use defuse_outlayer_host::crypto::secp256k1::{
     Secp256k1Host, Secp256k1PublicKey, Secp256k1Signature,
 };
+use k256::{
+    Secp256k1,
+    elliptic_curve::{CurveArithmetic, PrimeField},
+};
 
 use crate::WorkerHost;
 
 impl Secp256k1Host for WorkerHost {
-    fn secp256k1_derive_public_key(&self, _path: &str) -> Secp256k1PublicKey {
-        unimplemented!()
+    fn secp256k1_derive_public_key(&self, path: &str) -> Secp256k1PublicKey {
+        let tweak = [0u8; 32]; // TODO
+        let tweak = k256::Scalar::from_repr(tweak.into()).unwrap();
+
+        let pk = (<Secp256k1 as CurveArithmetic>::ProjectivePoint::GENERATOR * tweak
+            + self.secp256k1_root_sk.public_key().as_affine())
+        .to_affine();
+
+        k256::PublicKey::from_affine(pk)
+            .unwrap() // TODO
+            .to_sec1_bytes() // TODO
+            .to_vec()
+            .try_into()
+            .unwrap()
     }
 
     fn secp256k1_sign(&self, _path: &str, _msg: &[u8]) -> Secp256k1Signature {
--- crates/outlayer/worker-host/src/lib.rs
+++ crates/outlayer/worker-host/src/lib.rs
@@ -1,3 +1,41 @@
+use borsh::BorshSerialize;
+use hkdf::{Hkdf, HkdfExtract, Kdf};
+use k256::SecretKey as Secp256k1SecretKey;
+use near_account_id::AccountId;
+
 pub mod crypto;
 
-pub struct WorkerHost;
+pub struct WorkerHost {
+    app_id: AppId,
+    secp256k1_root_sk: Secp256k1SecretKey, // TODO: zeroize?
+}
+
+impl WorkerHost {
+    pub fn new(app_id: AppId, seed: &[u8]) -> Self {
+        // no salt is needed, seed is already with high entropy
+        let hk = Hkdf::<sha3::Sha3_512>::new(None, seed);
+
+        Self {
+            app_id,
+            secp256k1_root_sk: {
+                // TODO: SHA3-256 would have done the job in one round, too
+                let mut sk = [0u8; 32];
+                hk.expand(
+                    b"secp256k1", // TODO
+                    &mut sk,
+                )
+                .unwrap();
+                Secp256k1SecretKey::from_bytes(&sk.into())
+                    // TODO: handle zero
+                    .unwrap()
+            },
+        }
+    }
+}
+
+#[derive(BorshSerialize)]
+#[borsh(use_discriminant = true)]
+#[repr(u8)]
+pub enum AppId {
+    NearAccount(AccountId) = 0,
+}
