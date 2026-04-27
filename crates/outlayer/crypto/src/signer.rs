1e17578d7f81d2b6a99a8a24d5de21ec99328ea1
Arseny Mitin <mitinarseny@gmail.com>
2026-04-27 05:54:27 +0400

    chore: seed derivation tests

--- crates/outlayer/crypto/src/signer.rs
+++ crates/outlayer/crypto/src/signer.rs
@@ -6,7 +6,7 @@ use zeroize::ZeroizeOnDrop;
 
 use crate::{DerivableCurve, DeriveSigner};
 
-#[derive(Clone, ZeroizeOnDrop)]
+#[derive(Clone, ZeroizeOnDrop, PartialEq, Eq)]
 pub struct InMemorySigner {
     #[cfg(feature = "ed25519")]
     ed25519_root_sk: crate::ed25519::SigningKey,
@@ -15,35 +15,30 @@ pub struct InMemorySigner {
 }
 
 impl InMemorySigner {
-    const SALT: &'static [u8] = b"outlayer v0.1.0 ikm seed:";
+    const HKDF_SEED_SALT: &'static [u8] = b"outlayer v0.1.0 signer seed:";
+
+    #[cfg(feature = "ed25519")]
+    const HKDF_INFO_ED25519_ROOT_SK: &'static [u8] = b"ed25519/root_sk";
+    #[cfg(feature = "secp256k1")]
+    const HKDF_INFO_SECP256K1_ROOT_SK: &'static [u8] = b"secp256k1/root_sk";
 
     pub fn from_seed(seed: &[u8]) -> Self {
-        // no salt is needed, seed is already with high entropy
-        let hk = Hkdf::<Sha3_512>::new(Some(Self::SALT), seed);
+        let hk = Hkdf::<Sha3_512>::new(Some(Self::HKDF_SEED_SALT), seed);
 
         Self {
             #[cfg(feature = "ed25519")]
             ed25519_root_sk: {
-                let mut sk = ed25519_dalek::SecretKey::default();
-                hk.expand(
-                    b"ed25519", // TODO
-                    &mut sk,
-                )
-                .unwrap();
+                let mut sk = [0u8; ed25519_dalek::SECRET_KEY_LENGTH];
+                hk.expand(Self::HKDF_INFO_ED25519_ROOT_SK, &mut sk)
+                    .expect("HKDF: ed25519");
                 ed25519_dalek::SigningKey::from_bytes(&sk)
             },
             #[cfg(feature = "secp256k1")]
             secp256k1_root_sk: {
-                // TODO: SHA3-256 would have done the job in one round, too
                 let mut sk = [0u8; 32];
-                hk.expand(
-                    b"secp256k1", // TODO
-                    &mut sk,
-                )
-                .unwrap();
-                k256::SecretKey::from_bytes(&sk.into())
-                    // TODO: handle zero
-                    .unwrap()
+                hk.expand(Self::HKDF_INFO_SECP256K1_ROOT_SK, &mut sk)
+                    .expect("HKDF: expand");
+                k256::SecretKey::from_bytes(&sk.into()).expect("secp256k1: zero scalar")
             },
         }
     }
@@ -60,9 +55,9 @@ const _: () = {
 
         fn sign(
             &self,
-            tweak: &<Ed25519 as crate::DerivableCurve>::Tweak,
-            msg: &[u8],
-        ) -> <Ed25519 as crate::DerivableCurve>::Signature {
+            tweak: &<Ed25519 as DerivableCurve>::Tweak,
+            msg: &<Ed25519 as DerivableCurve>::Message,
+        ) -> <Ed25519 as DerivableCurve>::Signature {
             self.ed25519_root_sk.sign(tweak, msg)
         }
     }
@@ -79,9 +74,9 @@ const _: () = {
 
         fn sign(
             &self,
-            tweak: &<Secp256k1 as crate::DerivableCurve>::Tweak,
-            msg: &[u8; 32],
-        ) -> <Secp256k1 as crate::DerivableCurve>::Signature {
+            tweak: &<Secp256k1 as DerivableCurve>::Tweak,
+            msg: &<Secp256k1 as DerivableCurve>::Message,
+        ) -> <Secp256k1 as DerivableCurve>::Signature {
             self.secp256k1_root_sk.sign(tweak, msg)
         }
     }
@@ -89,5 +84,53 @@ const _: () = {
 
 #[cfg(test)]
 mod tests {
-    // TODO
+    use hex_literal::hex;
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case(
+        b"",
+        InMemorySigner {
+            #[cfg(feature = "ed25519")]
+            ed25519_root_sk: ed25519_dalek::SigningKey::from_bytes(
+                &hex!("87841790a661e9258a220a23598b1a15f54a8aaac9db0d160918153f8004c008"),
+            ),
+            #[cfg(feature = "secp256k1")]
+            secp256k1_root_sk: k256::SecretKey::from_bytes(
+                &hex!("954787a5fb30c67cb33717beecc4c0378e76f1142b6d5b7f9d168baa3a02c166").into(),
+            ).unwrap(),
+        }
+    )]
+    #[case(
+        b"test",
+        InMemorySigner {
+            #[cfg(feature = "ed25519")]
+            ed25519_root_sk: ed25519_dalek::SigningKey::from_bytes(
+                &hex!("7fef7fed7d4ef1f7b566c0d8eb25c979295279bf733c3e41aeb731a572f63a28"),
+            ),
+            #[cfg(feature = "secp256k1")]
+            secp256k1_root_sk: k256::SecretKey::from_bytes(
+                &hex!("fe897ab00ec1763cde63c7d96ecb841402c8f62dbb5d2ff5f8fea8e500a92c2d").into(),
+            ).unwrap(),
+        }
+    )]
+    #[case(
+        &hex!("f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2"),
+        InMemorySigner {
+            #[cfg(feature = "ed25519")]
+            ed25519_root_sk: ed25519_dalek::SigningKey::from_bytes(
+                &hex!("c846458ca3667e46a7dc2814713b99ef4b441523bbe2d286808025b10dab07a0"),
+            ),
+            #[cfg(feature = "secp256k1")]
+            secp256k1_root_sk: k256::SecretKey::from_bytes(
+                &hex!("64803a99628fd19eb82cc7cc8a41d9f1745eb7f7f1e2887058d3b922db93f74e").into(),
+            ).unwrap(),
+        }
+    )]
+    fn seed_derivation_has_not_changed(#[case] seed: &[u8], #[case] expected: InMemorySigner) {
+        let derived = InMemorySigner::from_seed(seed);
+        assert!(derived == expected, "seed derivation has changed");
+    }
 }
