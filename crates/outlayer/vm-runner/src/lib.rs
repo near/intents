2b517c5614a9426ce92aa46d286ab708199d6cb8
pityjllk <pityjllk@proton.me>
2026-05-02 14:18:25 +0200

    fix formatting

--- crates/outlayer/vm-runner/src/lib.rs
+++ crates/outlayer/vm-runner/src/lib.rs
@@ -9,6 +9,6 @@ pub use self::context::HostFunctions;
 pub use defuse_outlayer_host as host;
 
 pub use wasmtime;
-pub use wasmtime_wasi;
 pub use wasmtime::component::Component;
+pub use wasmtime_wasi;
 pub use wasmtime_wasi::p2::pipe::{MemoryInputPipe, MemoryOutputPipe};
