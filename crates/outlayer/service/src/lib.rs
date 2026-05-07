f40077a0aa04d3aa683c0b126afd7ac3c83b7b24
Arseny Mitin <mitinarseny@gmail.com>
2026-05-07 16:03:19 +0400

    feat(outlayer): service (#262)
    * feat(service): executor
    
    * wip
    
    * refactor(outlayer/host): HostView
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * refactor: no service for now
    
    * refactor: no service
    
    * wip
    
    * feat: derive serde
    
    * chore
    
    * wip
    
    * fix(outlayer/vm-runner): cli
    
    * chore
    
    * feat(outlayer): Component cache (#260)
    
    * cache compiled wasm
    
    * cleanup
    
    * improve ergonomics
    
    * improve errors
    
    * fix formatting
    
    * improve ergonomics
    
    * revert some changes
    
    * revert removals
    
    * revert changes
    
    * revert
    
    * ci checks
    
    * fix compilation signature
    
    * remove newtype wrapper around compilation error
    
    * from => from_code
    
    * cache based on code hash
    
    * remove Option<Cache>
    
    * simplify impl
    
    * chagne resolve singature
    
    * builder pattern
    
    * add builder
    
    * address claude suggestions
    
    * fix clippy
    
    * remove comment
    
    * improve comment
    
    * chore: do not download code if component is cached already
    
    ---------
    
    Co-authored-by: Arseny Mitin <mitinarseny@gmail.com>
    
    * feat(outlayer): limit download size (#261)
    
    * limit download size
    
    * simplify
    
    * refactor
    
    * prevent ram exhaust attack
    
    * chore: make it a bit simpler
    
    * fix ci
    
    * chore: fix
    
    ---------
    
    Co-authored-by: Arseny Mitin <mitinarseny@gmail.com>
    
    * fix: use base64 standard alphabet for data URLs
    
    * fix(outlayer/http): check max_len
    
    * chore
    
    * chore
    
    ---------
    
    Co-authored-by: pityjllk <pityjllk@proton.me>

---
+++ crates/outlayer/service/src/lib.rs
@@ -0,0 +1,106 @@
+mod builder;
+mod cache;
+mod code;
+mod resolver;
+
+pub use self::resolver::Resolver;
+pub use self::{builder::*, cache::*, code::*};
+
+use std::{convert, sync::Arc};
+
+use anyhow::Context as _;
+use bytes::Bytes;
+use defuse_outlayer_executor::{
+    self as executor, Component, Context, Executor, HostContext, Outcome,
+};
+use defuse_outlayer_primitives::AppId;
+use moka::future::Cache;
+
+#[derive(Clone)]
+pub struct Outlayer {
+    resolver: Resolver,
+    executor: Executor,
+    runtime_cache: Cache<[u8; 32], Component>,
+}
+
+impl Outlayer {
+    pub fn builder() -> OutlayerBuilder {
+        OutlayerBuilder::default()
+    }
+
+    async fn resolve_app(&self, app: Code<'_>) -> Result<(AppId<'static>, Component), Error> {
+        let (app_id, app_code_url, code) = match app {
+            Code::Ref(code_ref) => (
+                code_ref.app_id(),
+                self.resolver.resolve_code_url(code_ref).await?,
+                None,
+            ),
+            Code::Inline { code } => {
+                let app_code_url = AppCodeUrl::from_code(&code);
+                (app_code_url.immutable_app_id(), app_code_url, Some(code))
+            }
+        };
+
+        let component = self
+            .runtime_cache
+            .try_get_with(app_code_url.code_hash, async move {
+                let code = if let Some(code) = code {
+                    code
+                } else {
+                    self.resolver.resolve_code(app_code_url).await?
+                };
+
+                tokio::task::spawn_blocking({
+                    let compiler = self.executor.compiler();
+                    move || compiler.compile(code)
+                })
+                .await
+                .context("panicked")
+                .and_then(convert::identity) // TODO: .flatten()
+                .map_err(PrepareError::Compile)
+            })
+            .await?;
+
+        Ok((app_id, component))
+    }
+
+    pub async fn execute(&self, app: Code<'_>, input: Bytes, fuel: u64) -> Result<Outcome, Error> {
+        let (app_id, component) = self.resolve_app(app).await?;
+
+        self.executor
+            .execute(
+                Context {
+                    input,
+                    host: HostContext { app_id },
+                },
+                &component,
+                fuel,
+            )
+            .await
+            .map_err(Into::into)
+    }
+}
+
+#[derive(derive_more::From, thiserror::Error, Debug)]
+pub enum Error {
+    #[from(PrepareError)]
+    #[error("prepare: {0}")]
+    Prepare(#[from] Arc<PrepareError>),
+    #[error(transparent)]
+    Execute(#[from] executor::Error),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum PrepareError {
+    #[error("resolve: {0}")]
+    Resolve(#[from] resolver::Error),
+
+    #[error("compile: {0}")]
+    Compile(anyhow::Error),
+}
+
+impl From<resolver::Error> for Error {
+    fn from(err: resolver::Error) -> Self {
+        Self::Prepare(PrepareError::Resolve(err).into())
+    }
+}
