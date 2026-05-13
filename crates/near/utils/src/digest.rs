be05c1c281b7ac5bcd993abb7d6b46b63b44fdde
pityjllk <pityjllk@proton.me>
2026-05-13 04:24:09 +0200

    keccak256 digest

--- crates/near/utils/src/digest.rs
+++ crates/near/utils/src/digest.rs
@@ -31,6 +31,36 @@ impl FixedOutput for Sha256 {
 
 impl HashMarker for Sha256 {}
 
+#[derive(Debug, Clone, Default)]
+pub struct Keccak256 {
+    data: Vec<u8>,
+}
+
+impl Update for Keccak256 {
+    #[inline]
+    fn update(&mut self, data: &[u8]) {
+        self.data.extend(data);
+    }
+}
+
+impl OutputSizeUser for Keccak256 {
+    type OutputSize = U32;
+}
+
+impl FixedOutput for Keccak256 {
+    #[inline]
+    fn finalize_into(self, out: &mut digest::Output<Self>) {
+        *out = self.finalize_fixed();
+    }
+
+    #[inline]
+    fn finalize_fixed(self) -> digest::Output<Self> {
+        env::keccak256_array(&self.data).into()
+    }
+}
+
+impl HashMarker for Keccak256 {}
+
 #[cfg(test)]
 mod tests {
     use defuse_test_utils::random::random_bytes;
@@ -41,8 +71,14 @@ mod tests {
     use super::*;
 
     #[rstest]
-    fn digest(random_bytes: Vec<u8>) {
+    fn sha256(random_bytes: Vec<u8>) {
         let got: CryptoHash = Sha256::digest(&random_bytes).into();
         assert_eq!(got, env::sha256_array(&random_bytes));
+    }
+
+    #[rstest]
+    fn keccak256(random_bytes: Vec<u8>) {
+        let got: CryptoHash = Keccak256::digest(&random_bytes).into();
+        assert_eq!(got, env::keccak256_array(&random_bytes));
     }
 }
--- crates/primitives/digest/src/lib.rs
+++ crates/primitives/digest/src/lib.rs
@@ -1,13 +1,17 @@
 #[cfg(all(feature = "near-contract", feature = "sha2"))]
 compile_error!("features `near-contract` and `sha2` are mutually exclusive");
 
-#[cfg(not(any(feature = "near-contract", feature = "sha2")))]
-compile_error!("exactly one of `near-contract` or `sha2` must be enabled");
-
-pub use digest::Digest;
+#[cfg(all(feature = "near-contract", feature = "sha3"))]
+compile_error!("features `near-contract` and `sha3` are mutually exclusive");
 
 #[cfg(feature = "near-contract")]
 pub use defuse_near_utils::digest::Sha256;
 
 #[cfg(feature = "sha2")]
 pub use sha2::Sha256;
+
+#[cfg(feature = "near-contract")]
+pub use defuse_near_utils::digest::Keccak256;
+
+#[cfg(feature = "sha3")]
+pub use sha3::Keccak256;
