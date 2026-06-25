9d5ca7b0fbb10d32d314ba61b7d4ef2deacb5a27
Arseny Mitin <mitinarseny@gmail.com>
2026-06-25 13:54:29 +0400

    feat: borsh schema

--- crates/primitives/time/src/borsh.rs
+++ crates/primitives/time/src/borsh.rs
@@ -44,6 +44,30 @@ macro_rules! borsh_as {
                 jiff::Timestamp::$from(timestamp).map(Timestamp).map_err(io::Error::other)
             }
         }
+
+        #[cfg(feature = "abi")]
+        const _: () = {
+            use borsh::{BorshSchema, schema::{Declaration, Definition}};
+            use defuse_borsh_utils::adapters::BorshSchemaAs;
+
+            impl<I> BorshSchemaAs<Timestamp> for $name<I>
+            where
+                I: BorshSchema,
+            {
+                fn declaration_as() -> borsh::schema::Declaration {
+                    <I as BorshSchema>::declaration()
+                }
+
+                fn add_definitions_recursively_as(
+                    definitions: &mut std::collections::BTreeMap<
+                        Declaration,
+                        Definition,
+                    >,
+                ) {
+                    <I as BorshSchema>::add_definitions_recursively(definitions);
+                }
+            }
+        };
     )*};
 }
 
@@ -68,154 +92,6 @@ borsh_as! {
         from_nanosecond,
     }
 }
-
-// pub struct TimestampSeconds<I = i64>(PhantomData<I>);
-
-// impl<I> BorshSerializeAs<Timestamp> for TimestampSeconds<I>
-// where
-//     I: TryFrom<i64> + BorshSerialize,
-//     I::Error: Display,
-// {
-//     #[inline]
-//     fn serialize_as<W>(source: &Timestamp, writer: &mut W) -> io::Result<()>
-//     where
-//         W: io::Write,
-//     {
-//         // TODO
-//         I::try_from(source.as_second())
-//             .map_err(|err| io::Error::other(err.to_string()))?
-//             .serialize(writer)
-//     }
-// }
-
-// impl<I> BorshDeserializeAs<Timestamp> for TimestampSeconds<I>
-// where
-//     I: TryInto<i64> + BorshDeserialize,
-//     I::Error: Display,
-// {
-//     fn deserialize_as<R>(reader: &mut R) -> io::Result<Timestamp>
-//     where
-//         R: io::Read,
-//     {
-//         let timestamp: i64 = I::deserialize_reader(reader)?
-//             .try_into()
-//             .map_err(|err| io::Error::other(err.to_string()))?;
-//         Timestamp::from_second(timestamp).map_err(io::Error::other)
-//     }
-// }
-
-// pub struct TimestampMilliSeconds<I = i64>(PhantomData<I>);
-
-// impl<I> BorshSerializeAs<Timestamp> for TimestampMilliSeconds<I>
-// where
-//     I: TryFrom<i64> + BorshSerialize,
-//     I::Error: Display,
-// {
-//     #[inline]
-//     fn serialize_as<W>(source: &Timestamp, writer: &mut W) -> io::Result<()>
-//     where
-//         W: io::Write,
-//     {
-//         I::try_from(source.as_millisecond())
-//             .map_err(|err| io::Error::other(err.to_string()))?
-//             .serialize(writer)
-//     }
-// }
-
-// impl<I> BorshDeserializeAs<Timestamp> for TimestampMilliSeconds<I>
-// where
-//     I: TryInto<i64> + BorshDeserialize,
-//     I::Error: Display,
-// {
-//     fn deserialize_as<R>(reader: &mut R) -> io::Result<Timestamp>
-//     where
-//         R: io::Read,
-//     {
-//         let timestamp: i64 = I::deserialize_reader(reader)?
-//             .try_into()
-//             .map_err(|err| io::Error::other(err.to_string()))?;
-//         Timestamp::from_millisecond(timestamp).map_err(io::Error::other)
-//     }
-// }
-
-// pub struct TimestampMicroSeconds<I = i64>(PhantomData<I>);
-
-// impl<I> BorshSerializeAs<Timestamp> for TimestampMicroSeconds<I>
-// where
-//     I: TryFrom<i64> + BorshSerialize,
-//     I::Error: Display,
-// {
-//     #[inline]
-//     fn serialize_as<W>(source: &Timestamp, writer: &mut W) -> io::Result<()>
-//     where
-//         W: io::Write,
-//     {
-//         I::try_from(source.as_microsecond())
-//             .map_err(|err| io::Error::other(err.to_string()))?
-//             .serialize(writer)
-//     }
-// }
-
-// impl<I> BorshDeserializeAs<Timestamp> for TimestampMicroSeconds<I>
-// where
-//     I: TryInto<i64> + BorshDeserialize,
-//     I::Error: Display,
-// {
-//     fn deserialize_as<R>(reader: &mut R) -> io::Result<Timestamp>
-//     where
-//         R: io::Read,
-//     {
-//         let timestamp: i64 = I::deserialize_reader(reader)?
-//             .try_into()
-//             .map_err(|err| io::Error::other(err.to_string()))?;
-//         Timestamp::from_microsecond(timestamp).map_err(io::Error::other)
-//     }
-// }
-
-// pub struct TimestampNanoSeconds<I = i64>(PhantomData<I>);
-
-// impl<I> BorshSerializeAs<Timestamp> for TimestampNanoSeconds<I>
-// where
-//     I: TryFrom<i128> + BorshSerialize,
-//     I::Error: Display,
-// {
-//     #[inline]
-//     fn serialize_as<W>(source: &Timestamp, writer: &mut W) -> io::Result<()>
-//     where
-//         W: io::Write,
-//     {
-//         I::try_from(source.as_nanosecond())
-//             .map_err(|err| io::Error::other(err.to_string()))?
-//             .serialize(writer)
-//     }
-// }
-
-// impl<I> BorshDeserializeAs<Timestamp> for TimestampNanoSeconds<I>
-// where
-//     I: TryInto<i128> + BorshDeserialize,
-//     I::Error: Display,
-// {
-//     fn deserialize_as<R>(reader: &mut R) -> io::Result<Timestamp>
-//     where
-//         R: io::Read,
-//     {
-//         let timestamp: i128 = I::deserialize_reader(reader)?
-//             .try_into()
-//             .map_err(|err| io::Error::other(err.to_string()))?;
-//         Timestamp::from_nanosecond(timestamp).map_err(io::Error::other)
-//     }
-// }
-
-// TODO
-// #[cfg(feature = "abi")]
-// const _: () = {
-//     use crate::adapters::schema::impl_borsh_schema_as;
-
-//     impl_borsh_schema_as!(Timestamp, TimestampSeconds);
-//     impl_borsh_schema_as!(Timestamp, TimestampMilliSeconds);
-//     impl_borsh_schema_as!(Timestamp, TimestampMicroSeconds);
-//     impl_borsh_schema_as!(Timestamp, TimestampNanoSeconds);
-// };
 
 // TODO
 // #[cfg(test)]
