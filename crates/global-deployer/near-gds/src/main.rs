29194e53eec599b404f3fa000ad9d561f0ec9618
Arseny Mitin <mitinarseny@gmail.com>
2026-07-02 02:39:37 +0400

    chore

--- crates/global-deployer/near-gds/src/main.rs
+++ crates/global-deployer/near-gds/src/main.rs
@@ -27,7 +27,7 @@ struct Args {
     /// `HASH` can be encoded as base58 or hex with `0x` prefix.
     /// `@FILE` will calculate SHA-256 hash of the `FILE` contents.
     /// `@-` will calculate SHA-256 hash of the stdin contents.
-    #[arg(long, value_name = "HASH | @FILE | -")]
+    #[arg(long, value_name = "HASH | @FILE | @-")]
     pre_approve: Option<HashSource<Sha256>>,
 
     /// Output single-line JSON with base64-encoded keys/values
