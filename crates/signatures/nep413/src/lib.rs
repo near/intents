bbb50533b4d575c7738cd726f1dae9eda2f9e190
Arseny Mitin <mitinarseny@gmail.com>
2026-07-06 17:10:31 +0400

    wip: nep-413

--- crates/signatures/nep413/src/lib.rs
+++ crates/signatures/nep413/src/lib.rs
@@ -1,23 +1,24 @@
 use core::fmt::Display;
 
-use defuse_crypto::{Curve, Ed25519};
-use defuse_nep461::SignedMessageNep;
-use impl_tools::autoimpl;
+use anyhow::Context;
+use borsh::{BorshDeserialize, BorshSerialize};
+use defuse_digest::{Digest, sha2::Sha256};
+use defuse_nep461::{OffchainMessage, SignedMessageNep};
+use defuse_signature_schema::{Result, Schema};
+use digest_io::IoWrapper;
 
-/// See [NEP-413](https://github.com/near/NEPs/blob/master/neps/nep-0413.md)
-#[cfg_attr(
-    feature = "borsh",
-    derive(::borsh::BorshSerialize, ::borsh::BorshDeserialize),
-    cfg_attr(feature = "abi", derive(::borsh::BorshSchema))
-)]
 #[cfg_attr(
     feature = "serde",
     ::cfg_eval::cfg_eval,
     ::serde_with::serde_as,
     derive(::serde::Serialize, ::serde::Deserialize),
-    cfg_attr(feature = "abi", derive(::schemars::JsonSchema)),
+    cfg_attr(feature = "schemars-v0_8", derive(::schemars::JsonSchema)),
     serde(rename_all = "camelCase")
 )]
+#[derive(BorshSerialize, BorshDeserialize)]
+#[cfg_attr(feature = "borsh-schema", derive(::borsh::BorshSchema))]
+/// [NEP-413](https://github.com/near/NEPs/blob/master/neps/nep-0413.md)
+/// Offchain Signing Standard
 #[derive(Debug, Clone)]
 pub struct Nep413Payload {
     pub message: String,
@@ -34,16 +35,13 @@ pub struct Nep413Payload {
     pub callback_url: Option<String>,
 }
 
-impl SignedMessageNep for Nep413Payload {
-    const NEP_NUMBER: u32 = 413;
-}
-
 impl Nep413Payload {
+    #[must_use]
     #[inline]
-    pub fn new(message: String) -> Self {
+    pub fn new(message: impl Into<String>) -> Self {
         Self {
-            message,
-            nonce: Default::default(),
+            message: message.into(),
+            nonce: [0u8; 32],
             recipient: String::new(),
             callback_url: None,
         }
@@ -51,17 +49,14 @@ impl Nep413Payload {
 
     #[must_use]
     #[inline]
-    pub const fn with_nonce(mut self, nonce: [u8; 32]) -> Self {
-        self.nonce = nonce;
+    pub fn with_nonce(mut self, nonce: impl Into<[u8; 32]>) -> Self {
+        self.nonce = nonce.into();
         self
     }
 
     #[must_use]
     #[inline]
-    pub fn with_recipient<S>(mut self, recipient: S) -> Self
-    where
-        S: Display,
-    {
+    pub fn with_recipient<S>(mut self, recipient: impl Display) -> Self {
         self.recipient = recipient.to_string();
         self
     }
@@ -74,67 +69,38 @@ impl Nep413Payload {
     }
 }
 
-#[cfg(feature = "borsh")]
-impl defuse_crypto::Payload for Nep413Payload {
-    #[inline]
-    fn hash(&self) -> defuse_crypto::CryptoHash {
-        use defuse_digest::{Digest, sha2::Sha256};
-        use defuse_nep461::OffchainMessage;
-        use digest_io::IoWrapper;
+pub struct Nep413;
 
+impl SignedMessageNep for Nep413 {
+    const NEP_NUMBER: u32 = 413;
+}
+
+impl Schema<Nep413Payload> for Nep413 {
+    type Output = [u8; 32];
+
+    #[inline]
+    fn derive(&self, payload: Nep413Payload) -> Result<Self::Output> {
+        self.derive(&payload)
+    }
+}
+
+impl Schema<&Nep413Payload> for Nep413 {
+    type Output = [u8; 32];
+
+    fn derive(&self, payload: &Nep413Payload) -> Result<Self::Output> {
         let mut hasher = IoWrapper(Sha256::new());
+
         // serialize directly to hasher
-        borsh::to_writer(&mut hasher, &(Self::OFFCHAIN_PREFIX_TAG, self)).expect("borsh");
-        hasher.0.finalize().into()
-    }
-}
+        borsh::to_writer(&mut hasher, &(Self::OFFCHAIN_PREFIX_TAG, payload)).context("borsh")?;
 
-#[cfg_attr(
-    feature = "serde",
-    ::cfg_eval::cfg_eval,
-    ::serde_with::serde_as,
-    derive(::serde::Serialize, ::serde::Deserialize),
-    cfg_attr(feature = "abi", derive(::schemars::JsonSchema))
-)]
-#[autoimpl(Deref using self.payload)]
-#[derive(Debug, Clone)]
-pub struct SignedNep413Payload {
-    pub payload: Nep413Payload,
-
-    #[cfg_attr(
-        feature = "serde",
-        serde_as(as = "defuse_crypto::serde::AsCurve<Ed25519>")
-    )]
-    pub public_key: <Ed25519 as Curve>::PublicKey,
-    #[cfg_attr(
-        feature = "serde",
-        serde_as(as = "defuse_crypto::serde::AsCurve<Ed25519>")
-    )]
-    pub signature: <Ed25519 as Curve>::Signature,
-}
-
-#[cfg(feature = "borsh")]
-impl defuse_crypto::Payload for SignedNep413Payload {
-    #[inline]
-    fn hash(&self) -> defuse_crypto::CryptoHash {
-        self.payload.hash()
-    }
-}
-
-#[cfg(feature = "near-contract")]
-impl defuse_crypto::SignedPayload for SignedNep413Payload {
-    type PublicKey = <Ed25519 as Curve>::PublicKey;
-
-    #[inline]
-    fn verify(&self) -> Option<Self::PublicKey> {
-        use defuse_crypto::{Payload, VerifiableCurve};
-        Ed25519::verify(&self.signature, &self.hash(), &self.public_key)
+        Ok(hasher.0.finalize().into())
     }
 }
 
 #[cfg(feature = "near-kit")]
 const _: () = {
     impl From<Nep413Payload> for near_kit::nep413::SignMessageParams {
+        #[inline]
         fn from(payload: Nep413Payload) -> Self {
             Self {
                 message: payload.message,
@@ -146,3 +112,4 @@ const _: () = {
         }
     }
 };
+// TODO: tests
