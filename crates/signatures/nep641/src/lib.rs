ba04a3d7e2e25788c442aca72faa419f0e7d03d8
Arseny Mitin <mitinarseny@gmail.com>
2026-08-05 23:07:09 +0400

    chore: no borsh

--- crates/signatures/nep641/src/lib.rs
+++ crates/signatures/nep641/src/lib.rs
@@ -280,11 +280,6 @@ impl Extend<PendingAuthorization> for AuthorizationResolution {
     cfg_attr(feature = "schemars-v0_8", derive(::schemars::JsonSchema))
 )]
 #[cfg_attr(feature = "arbitrary", derive(::arbitrary::Arbitrary))]
-#[cfg_attr(
-    feature = "borsh",
-    derive(::borsh::BorshSerialize, ::borsh::BorshDeserialize),
-    cfg_attr(feature = "borsh-schema", derive(::borsh::BorshSchema))
-)]
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct PendingAuthorization {
     /// Account ID to [resolve](AuthResolver::w_resolve_auth) the
