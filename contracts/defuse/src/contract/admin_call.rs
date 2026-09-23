a329f7ee12f2f12b8b66c32fb2c67f3508d7884f
hlgltvnnk <olalytvinenko@gmail.com>
2026-09-23 12:40:30 +0300

    near promises used in defuse action

--- contracts/defuse/src/contract/admin_call.rs
+++ contracts/defuse/src/contract/admin_call.rs
@@ -1,6 +1,6 @@
 use defuse_near_promise::{NearPromise, actions::NearAction};
 use near_plugins::{AccessControllable, access_control_any};
-use near_sdk::{AccountId, Promise, env, near, require};
+use near_sdk::{env, near, require};
 
 use super::{Contract, ContractExt, Role};
 use crate::admin_call::AdminCallManager;
@@ -9,22 +9,26 @@ use crate::admin_call::AdminCallManager;
 impl AdminCallManager for Contract {
     #[access_control_any(roles(Role::DAO))]
     #[payable]
-    fn admin_call(&mut self, receiver_id: AccountId, action: NearAction) -> Promise {
+    fn admin_call(&mut self, promises: Vec<NearPromise>) {
         require!(
             !env::attached_deposit().is_zero(),
             "requires attached deposit of at least 1 yoctoNEAR"
         );
 
-        require!(
-            matches!(
-                action,
-                NearAction::FunctionCall(_) | NearAction::Transfer(_)
-            ),
-            "unsupported action"
-        );
+        for p in promises {
+            require!(
+                p.actions.iter().all(|action| {
+                    matches!(
+                        action,
+                        NearAction::FunctionCall(_) | NearAction::Transfer(_)
+                    )
+                }),
+                "unsupported action"
+            );
 
-        // TODO: Given that it is allowed to spend contract balance by arbitrary call,
-        // should refund go to the intents contract in case of failure?
-        NearPromise::new(receiver_id).add_action(action).build()
+            // TODO: Given that it is allowed to spend contract balance by arbitrary call,
+            // refund goes to the intents contract in case of failure
+            p.build().detach();
+        }
     }
 }
